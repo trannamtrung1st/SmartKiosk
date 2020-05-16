b0890d54aef613cc1b01a34199dcf66c46d9b316
trannamtrung1st <trannamtrung1st@gmail.com>
2020-05-16 15:39:45 +0700

    add schedule service

--- SK.Business/Models/ScheduleModels.cs
+++ SK.Business/Models/ScheduleModels.cs
@@ -1,4 +1,5 @@
-﻿using SK.Data.Models;
+﻿using Newtonsoft.Json;
+using SK.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,44 @@ using System.Threading.Tasks;
 
 namespace SK.Business.Models
 {
+    public class CreateScheduleModel : MappingModel<Schedule>
+    {
+        public CreateScheduleModel()
+        {
+        }
+
+        public CreateScheduleModel(Schedule src) : base(src)
+        {
+        }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("location_id")]
+        public int LocationId { get; set; }
+    }
+
+    public class UpdateScheduleModel : MappingModel<Schedule>
+    {
+        public UpdateScheduleModel()
+        {
+        }
+
+        public UpdateScheduleModel(Schedule src) : base(src)
+        {
+        }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+
     #region Query
     public class ScheduleRelationship : Schedule, IDapperRelationship
     {
---
+++ SK.Business/Services/ScheduleService.cs
@@ -0,0 +1,50 @@
+﻿using SK.Business.Models;
+using SK.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TNT.Core.Helpers.DI;
+
+namespace SK.Business.Services
+{
+    public class ScheduleService : Service
+    {
+        public ScheduleService(ServiceInjection inj) : base(inj)
+        {
+        }
+
+        #region Query Schedule
+        public IQueryable<Schedule> Schedules
+        {
+            get
+            {
+                return context.Schedule;
+            }
+        }
+        #endregion
+
+        #region Create Schedule
+        protected void PrepareCreate(Schedule entity)
+        {
+        }
+
+        public Schedule CreateSchedule(CreateScheduleModel model)
+        {
+            var entity = model.ToDest();
+            PrepareCreate(entity);
+            return context.Schedule.Add(entity).Entity;
+
+        }
+        #endregion
+
+        #region Update Schedule
+        public void UpdateSchedule(Schedule entity, UpdateScheduleModel model)
+        {
+            model.CopyTo(entity);
+        }
+
+        #endregion
+
+    }
+}
