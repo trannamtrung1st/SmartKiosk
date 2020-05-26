5221afef805082a4c988e0dfeb32e927d66753df
trannamtrung1st <trannamtrung1st@gmail.com>
2020-05-26 22:22:58 +0700

    finish detail

---
+++ SK.WebAdmin/Areas/Location/Pages/Post/Detail.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SK.WebAdmin.Helpers;
+using SK.WebAdmin.Models;
+
+namespace SK.WebAdmin.Areas.Location.Pages.Post
+{
+    public class DetailModel : LocationPageModel<DetailModel>
+    {
+        public int Id { get; set; }
+        public void OnGet(int id)
+        {
+            SetPageInfo();
+            Id = id;
+        }
+
+        protected override void SetPageInfo()
+        {
+            Info = new PageInfo
+            {
+                Title = "Chi tiết bài viết",
+                Menu = Menu.LOCATION_POST,
+                BackUrl = BackUrl ?? Routing.LOCATION_POST.LocId(LocId)
+            };
+        }
+    }
+}
